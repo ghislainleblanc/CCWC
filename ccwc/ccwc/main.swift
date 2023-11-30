363406c73b771adaf9d06448382a1b061c629349
Ghislain Leblanc <ghisleb@me.com>
2023-11-30 11:19:19 -0500

    Handle command line -c

--- ccwc/ccwc/main.swift
+++ ccwc/ccwc/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Option: String {
+    case byteCount = "-c"
+}
 
+func handleCommandLineArguments() -> Option? {
+    let arguments = CommandLine.arguments
+
+    var index = 1
+    while index < arguments.count {
+        guard let option = Option(rawValue: arguments[index]) else {
+            print("Unknown option: \(arguments[index])")
+            return nil
+        }
+
+        switch option {
+        case .byteCount:
+            index += 1
+            return .byteCount
+        }
+    }
+
+    return nil
+}
+
+if let option = handleCommandLineArguments() {
+    print("Byte count")
+}
